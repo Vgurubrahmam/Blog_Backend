ef5159b0b5db86e43e1c705a2952f6612dbbe525
gurubrahmam <vgurubrahmam338@gmail.com>
2025-01-24 09:21:19 +0530

    Cors err

--- api/index.js
+++ api/index.js
@@ -9,7 +9,12 @@ const Comments =require("../models/commentSchema")
 const app = express();
 app.use(express.json());
 
-app.use(cors());
+app.use(cors({
+  origin : "https://blogpost-kappa-six.vercel.app/" ,
+  credentials : true ,
+  allowedHeaders :  ["Content-Type", "Authorization", "X-Requested-With", "Accept"],
+  methods : ["GET" , "POST" , "PUT" , "DELETE"]
+}));
 
 const secretKey = "guru";
 
